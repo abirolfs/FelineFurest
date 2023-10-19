911c5d404b88172cf2c74c743ead935057f5f9e1
abirolfs <abbrolfs@iu.edu>
2023-10-19 18:31:16 -0400

    start tool use

--- Feline-Furest-Spa/Assets/Scripts/Tool.cs
+++ Feline-Furest-Spa/Assets/Scripts/Tool.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class Tool : MonoBehaviour
 {
+    [SerializeField] int toolUseCount = 0;
+
     [SerializeField] bool isPickedUp = false;
-    [SerializeField] bool isInSquare = true;
+    //[SerializeField] bool isInSquare = true;
 
     [SerializeField] float zPosition = -1.5f;
 
     [SerializeField] GameObject toolSquare;
+    [SerializeField] GameObject cat;
     [SerializeField] BoxCollider2D toolCollider;
     [SerializeField] BoxCollider2D squareCollider;
-    Bounds toolBounds;
-    Bounds squareBounds;
+    [SerializeField] BoxCollider2D catCollider;
+
+    //Bounds toolBounds;
+    //Bounds squareBounds;
 
     // Start is called before the first frame update
     void Start()
     {
         toolCollider = this.GetComponent<BoxCollider2D>();
         squareCollider = toolSquare.GetComponent<BoxCollider2D>();
+        cat = GameObject.FindGameObjectWithTag("Cat");
+        catCollider = cat.GetComponent<BoxCollider2D>();
 
         //toolBounds = toolCollider.bounds;
         //toolBounds.extents += 10;
@@ -44,8 +51,6 @@ public class Tool : MonoBehaviour
     private void OnMouseDown()
     {
 
-
-
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePosition.x, mousePosition.y, zPosition);
 
@@ -59,16 +64,16 @@ public class Tool : MonoBehaviour
         {
             isPickedUp = true;
         }
-        //else if (isPickedUp = true && toolCollider.bounds.Intersects(squareCollider.bounds))
-        //{
-        //    Debug.Log("Tool intersecting Tool Square!");
-        //    transform.position = new Vector3(toolSquare.transform.position.x, toolSquare.transform.position.y, zPosition);
-        //    isPickedUp = false;
-        //}
+        //logic for the tool being clicked outside of toolsquare - AKA actually using the tool
         else
         {
-            //logic for the tool being picked up but not in bounds of tool square goes here
-            //isPickedUp = false;
+
+            if (catCollider.bounds.Contains(new Vector3(mousePosition.x, mousePosition.y, cat.transform.position.z)))
+            {
+                Debug.Log("Used tool on cat!");
+                toolUseCount++;
+            }
+
         }
     }
 }
