3e046d0c331a6213f4f6bb0eb2a3517be2eaf628
abirolfs <abbrolfs@iu.edu>
2023-10-19 18:07:36 -0400

    switching steps functionality

--- Feline-Furest-Spa/Assets/Scripts/GameManager.cs
+++ Feline-Furest-Spa/Assets/Scripts/GameManager.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
+    [SerializeField] bool isOnShowerStep = true;
+    [SerializeField] bool isOnGroomStep = false;
+
     [SerializeField] GameObject waterBucket;
     [SerializeField] GameObject shampooBottle;
+    [SerializeField] GameObject towel;
+
+    //[SerializeField] GameObject backButton;
+    //[SerializeField] GameObject nextButton;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        towel.SetActive(false);
+        //backButton.GetComponent<Button>().enabled = false;
     }
 
     // Update is called once per frame
@@ -19,4 +28,50 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void GoToNextStep()
+    {
+
+        if (isOnShowerStep)
+        {
+            isOnShowerStep = false;
+            isOnGroomStep = true;
+            waterBucket.SetActive(false);
+            shampooBottle.SetActive(false);
+            towel.SetActive(true);
+            //backButton.enabled = true;
+        }
+        //logic for any further step switches goes here
+    }
+
+    public void GoBackToLastStep()
+    {
+        if (isOnGroomStep)
+        {
+            isOnShowerStep = true;
+            isOnGroomStep = false;
+            waterBucket.SetActive(true);
+            shampooBottle.SetActive(true);
+            towel.SetActive(false);
+        }
+    }
+
+    //public void SwitchTools()
+    //{
+    //    if (isOnShowerStep)
+    //    {
+    //        isOnShowerStep = false;
+    //        waterBucket.SetActive(false);
+    //        shampooBottle.SetActive(false);
+    //        towel.SetActive(true);
+    //        //backButton.enabled = true;
+    //    }
+    //    else
+    //    {
+    //        isOnShowerStep = true;
+    //        waterBucket.SetActive(true);
+    //        shampooBottle.SetActive(true);
+    //        towel.SetActive(false);
+    //    }
+    //}
 }
